86ee4589e1c2e28987d1e2ca37e7c035d1f5b5a4
yunusemreyilmaz93-pixel <yunusemreyilmaz93@gmail.com>
2026-09-07 22:24:11 +0300

    Add curated CC0 Kenney room asset manifest

---
+++ components/room/assets.ts
@@ -0,0 +1,10 @@
+export const KENNEY_FURNITURE_BASE =
+  "https://raw.githubusercontent.com/ETdoFresh/kenney.nl/master/furniturekit_updated/Models/GLTF%20format";
+
+export const roomAssets = {
+  chairDesk: `${KENNEY_FURNITURE_BASE}/chairDesk.glb`,
+  bookcaseOpen: `${KENNEY_FURNITURE_BASE}/bookcaseOpen.glb`,
+  books: `${KENNEY_FURNITURE_BASE}/books.glb`,
+  keyboard: `${KENNEY_FURNITURE_BASE}/computerKeyboard.glb`,
+  mouse: `${KENNEY_FURNITURE_BASE}/computerMouse.glb`,
+} as const;
